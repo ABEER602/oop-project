ccf285f5b6c4f1caf9c54180dc15e57742eed737
ABEER602 <120298211+ABEER602@users.noreply.github.com>
2023-06-09 22:40:19 +0500

    week 3

---
+++ task 1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task__week_3_.classes;
+
+namespace task__week_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  for object 1
+            user s1 = new user("JOHN");
+            Console.WriteLine("OBJECT 1");
+
+            Console.WriteLine("Name: " + s1.sname);
+            Console.WriteLine();
+
+            //  for object 2
+            Console.WriteLine("OBJECT 2");
+         
+            user s2 = new user("Jack");
+            Console.WriteLine("Name: " + s2.sname);
+            Console.Read();
+         
+        }
+    }
+}
---
+++ task 2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task2_week_3_lab_.classes;
+
+namespace task2_week_3_lab_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //----------Task 2-------(printing attributes using default constructor)-------
+         //  for object 1
+            student s1 = new student();
+            Console.WriteLine("OBJECT 1");
+            Console.WriteLine();
+            Console.WriteLine("Name: " + s1.sname);
+            Console.WriteLine("Matric marks: " + s1.matricMarks);
+            Console.WriteLine("FSC marks: " + s1.fscMarks);
+            Console.WriteLine("ECAT marks: " + s1.ecatMarks);
+            Console.WriteLine("Agregate marks: " + s1.agregate);
+            //  for object 2
+            Console.WriteLine("OBJECT 2");
+            Console.WriteLine();
+            student s2 = new student();
+            Console.WriteLine("Name: " + s2.sname);
+            Console.WriteLine("Matric marks: " + s2.matricMarks);
+            Console.WriteLine("FSC marks: " + s2.fscMarks);
+            Console.WriteLine("ECAT marks: " + s2.ecatMarks);
+            Console.WriteLine("Agregate marks: " + s2.agregate);
+            //  for object 3
+            Console.WriteLine("OBJECT 3");
+            Console.WriteLine();
+            student s3 = new student();
+            Console.WriteLine("Name: " + s3.sname);
+            Console.WriteLine("Matric marks: " + s3.matricMarks);
+            Console.WriteLine("FSC marks: " + s3.fscMarks);
+            Console.WriteLine("ECAT marks: " + s3.ecatMarks);
+            Console.WriteLine("Agregate marks: " + s3.agregate);
+            Console.Read();
+        }
+    }
+}
